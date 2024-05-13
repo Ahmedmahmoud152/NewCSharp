006ab3b740af2f686edee9d0b180c0a3869f7ed3
Ahmedmahmoud152 <148643192+Ahmedmahmoud152@users.noreply.github.com>
2024-05-14 02:37:07 +0300

    easy
    c# wait you

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, C#Ahmed");
+             Console.WriteLine("C#,waiting you for star let's go");
         }
     }
 }
